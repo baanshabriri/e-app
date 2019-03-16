7d9e32b2208297db546723a04fe9fbf66ccf4645
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2019-03-17 00:19:49 +0530

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,23 +4,21 @@ import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
 
 import { AppRoutingModule } from './app-routing.module';
-import { NavBarComponent } from './nav-bar/nav-bar.component';
 import { AppComponent } from './app.component';
 import { HomeComponent } from './home/home.component';
 import { AboutUsComponent } from './about-us/about-us.component';
 import { BlogComponent } from './blog/blog.component';
-import { FooterComponent } from './footer/footer.component';
 
 import { SlickCarouselModule } from 'ngx-slick-carousel';
+import { BackpackComponent } from './backpack/backpack.component';
 
 @NgModule({
   declarations: [
-    NavBarComponent,
     AppComponent,
     HomeComponent,
     AboutUsComponent,
     BlogComponent,
-    FooterComponent
+    BackpackComponent,
   ],
   imports: [
     HttpClientModule,
--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -11,10 +11,10 @@ import * as $ from 'jquery';
 export class HomeComponent implements OnInit {
   public group = [];
   public groupChunk = [];
-  public subGroup:Number = 3;
+  public subGroup: Number = 3;
   showPost: boolean = false;
   displayData = [];
-  counter:number = 1;
+  counter: number = 1;
 
   constructor() { }
 
@@ -22,38 +22,44 @@ export class HomeComponent implements OnInit {
 
     this.cardF(),
 
-      $("#sidebarToggle").click(function(){
-       $("#mySidenav").width("20vw");
-       $("#mySidenav").animate({opacity:1},200)
-     })
- 
-     $("#sidebarBack").click(function(){
-       $("#mySidenav").width("0px")
-       $(".main").animate({opacity:1},200)
-     })
-    
-     $("#dropdown").hover(function(){
-       $("#menu").animate({opacity:1},500)
-     }, function() {
-      $("#menu").animate({opacity:0,display:"none"},500)
-     })
+      $("#sidebarToggle").click(function () {
+        $("#mySidenav").width("20vw");
+        $("#mySidenav").animate({ opacity: 1 }, 200)
+      })
 
-     $("#menu li").hover(function(){
-       $("#menu li").css({visibility: 'visible',
-        transform: 'scaleX('+1+')'})
-     })
-
-     $("#dropdown2").hover(function(){
-      $("#menu2").animate({opacity:1},500)
-    }, function() {
-     $("#menu2").animate({opacity:0,display:"none"},500)
+    $("#sidebarBack").click(function () {
+      $("#mySidenav").width("0px")
+      $(".main").animate({ opacity: 1 }, 200)
     })
 
-    $("#menu2 li").hover(function(){
-      $("#menu2 li").css({visibility: 'visible',
-       transform: 'scaleX('+1+')'})
+    $("#dropdown").hover(function () {
+      $("#menu").animate({ opacity: 1 }, 500)
+    }, function () {
+      $("#menu").animate({ opacity: 0, display: "none" }, 500)
+    })
+
+    $("#menu li").hover(function () {
+      $("#menu li").css({
+        visibility: 'visible',
+        transform: 'scaleX(' + 1 + ')'
+      })
+    })
+
+    $("#dropdown2").hover(function () {
+      $("#menu2").animate({ opacity: 1 }, 500)
+    }, function () {
+      $("#menu2").animate({ opacity: 0, display: "none" }, 500)
+    })
+
+    $("#menu2 li").hover(function () {
+      $("#menu2 li").css({
+        visibility: 'visible',
+        transform: 'scaleX(' + 1 + ')'
+      })
     })
   }
+
+
 
   public card = [
     {
@@ -140,7 +146,7 @@ export class HomeComponent implements OnInit {
       cardTitle: "hello",
       cardInfo: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
       cardLink: "../../assets/trekking-hiking-group-alpine-53214.jpeg"
-    }, 
+    },
     {
       cardTitle: "hello",
       cardInfo: "This is a longer card with supporting text below as a natural lead-in to additional content. This content is a little bit longer.",
@@ -168,9 +174,6 @@ export class HomeComponent implements OnInit {
     }
   ]
 
-  /**
-   * card:any
-   */
   public cardF: any = () => {
     for (let i = 0; i < this.card.length; i++) {
       if (i >= this.card.length - 1) {
@@ -194,15 +197,15 @@ export class HomeComponent implements OnInit {
   }
 
   public getPostList = () => {
-    if(this.counter < this.group.length){
-      for (let i = this.counter; i < this.counter+1; i++) {
+    if (this.counter < this.group.length) {
+      for (let i = this.counter; i < this.counter + 1; i++) {
         console.log(this.displayData);
         this.displayData.push(this.groupChunk[i])
       }
-      this.counter+=1
+      this.counter += 1
     }
-      else{
-        
-      }
+    else {
+
+    }
   }
 }
