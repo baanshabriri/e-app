f217eb7bc07e19b22083b08cc5e5d38e5a2a9f00
Vishal Dutta <36704086+vdvishal@users.noreply.github.com>
2019-03-19 02:49:19 +0530

    #

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,6 +2,7 @@ import { BrowserModule } from '@angular/platform-browser';
 import { NgModule } from '@angular/core';
 import { HttpClientModule } from '@angular/common/http'
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations'
+import { SlickModule } from 'ngx-slick';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
@@ -23,6 +24,7 @@ import { BackpackComponent } from './backpack/backpack.component';
     AppRoutingModule,
     BrowserModule,
     BrowserAnimationsModule,
+    SlickModule.forRoot()
   ],
   providers: [],
   bootstrap: [AppComponent]
--- src/app/backpack/backpack.component.ts
+++ src/app/backpack/backpack.component.ts
@@ -22,33 +22,41 @@ export class BackpackComponent implements OnInit {
     {
       link:"../../assets/backpack-bag-1546003.png",
       title:"Bag Title",
-      price:"123",
+      price:"1",
       info:"A brief Info"
     },
     {
       link:"../../assets/backpack-bag-1546003.png",
       title:"Bag Title",
-      price:"123",
+      price:"2",
       info:"A brief Info"
     },
     {
       link:"../../assets/backpack-bag-1546003.png",
       title:"Bag Title",
-      price:"123",
+      price:"3",
       info:"A brief Info"
     },
     {
       link:"../../assets/backpack-bag-1546003.png",
       title:"Bag Title",
-      price:"123",
+      price:"4",
       info:"A brief Info"
     },
     {
       link:"../../assets/backpack-bag-1546003.png",
       title:"Bag Title",
-      price:"123",
+      price:"5",
       info:"A brief Info"
     }
   ]
+  // slideConfig = {'slidesToShow': 3, 'slidesToScroll': 1};
+  slideConfig = {
+    "slidesToShow":3,
+    "slidesToScroll": 1,
+    "infinite": true,
+};  
 
+
+  
 }
